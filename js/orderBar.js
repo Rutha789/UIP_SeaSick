8e041526c8ed11ea1fe4a96c413e66ae6d9f28ab
tokeifung14 <keifung.to.0237@student.uu.se>
2020-02-19 14:27:13 +0100

    update orderBar and shopItem

--- js/orderBar.js
+++ js/orderBar.js
@@ -1,33 +1,42 @@
 $(document).ready(function(){
     var h = document.getElementById('cartList').clientHeight;
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
-    renderOrderItem(h);
+    //dummy test data
+
+    // if (typeof(Storage) !== "undefined") {
+    //     //     // Store
+    //     //     localStorage.setItem("lastname", "Smith");
+    //     //     // Retrieve
+    //     //     document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+    //     // }
+
+    var itemList;
+    itemList = localStorage.item
+    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
 });
 
-function renderOrderItem(h){
+function renderOrderItem(h,itemId,quan){
+
+    //render shopitem regarding the size
     var shopItem = document.createElement('div');
     shopItem.className = "shopItem";
-    shopItem.style = "height:"+h+"px;width:"+h+"px;display: inline-block;";
+    shopItem.style = "height:"+h+"px;width:"+h+"px;display: inline-block;padding:1% 1% 1% 1%";
     shopItem.draggable = true;
+    shopItem.ondrag = true;
+    if(itemId != undefined){shopItem.id = itemId;}
+
+    //create a infoicon for each shopItem
     var infoIcon = document.createElement("img");
     infoIcon.className = "infoIcon";
     infoIcon.style = "height:20%;width: 20%;top:0;margin: 2% 2% 2% 2%;";
     infoIcon.draggable = false;
     infoIcon.src = "../res/info_icon.png";
     shopItem.appendChild(infoIcon);
+
+    //append the item into the order item list
     document.getElementById('cartList').appendChild(shopItem);
+
+    //create a drag and drop overlay
+    var overlay = document.getElementById("overlay");
+    shopItem.addEventListener("drag", function(){overlay.style = "display:inherit;";});
+    shopItem.addEventListener("dragend", function(){overlay.style = "display:none;";});
 }
