291ebd22579cec4173e3a506daf45eac2372004b
tokeifung14 <keifung.to.0237@student.uu.se>
2020-02-22 05:01:34 +0100

    update orderBar and shopItem

--- js/orderBar.js
+++ js/orderBar.js
@@ -1,25 +1,18 @@
 $(document).ready(function(){
     var h = document.getElementById('cartList').clientHeight;
-    //dummy test data
-
-    // if (typeof(Storage) !== "undefined") {
-    //     //     // Store
-    //     //     localStorage.setItem("lastname", "Smith");
-    //     //     // Retrieve
-    //     //     document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-    //     // }
 
     var itemList;
     itemList = localStorage.item
     renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
 });
 
-function renderOrderItem(h,itemId,quan){
+function renderOrderItem(h,itemId,quan = 1, pc = "40%"){
+
 
     //render shopitem regarding the size
     var shopItem = document.createElement('div');
     shopItem.className = "shopItem";
-    shopItem.style = "height:"+h+"px;width:"+h+"px;display: inline-block;padding:1% 1% 1% 1%";
+    shopItem.style = "height:"+h+"px;width:"+h+"px;display: inline-block;padding:1% 1% 1% 1%;background-color: black;";
     shopItem.draggable = true;
     shopItem.ondrag = true;
     if(itemId != undefined){shopItem.id = itemId;}
@@ -32,11 +25,52 @@ function renderOrderItem(h,itemId,quan){
     infoIcon.src = "../res/info_icon.png";
     shopItem.appendChild(infoIcon);
 
-    //append the item into the order item list
-    document.getElementById('cartList').appendChild(shopItem);
+    //create a ecoIcon
+    var ecoIcon = document.createElement("img");
+    ecoIcon.className = "ecoIcon";
+    ecoIcon.style = "height:20%;width: 20%;bottom:0;margin: 2% 2% 2% 2%;";
+    ecoIcon.draggable = false;
+    ecoIcon.src = "../res/eco_icon.png";
+    shopItem.appendChild(ecoIcon);
+
+    //create a pcent
+    var pcent = document.createElement("p");
+    pcent.className = "pcent";
+    pcent.textContent = pc;
+    pcent.style = "height:20%;width: 20%;margin: 2% 2% 2% 2%;color:white;font-weight: bold;";
+    pcent.draggable = false;
+    shopItem.appendChild(pcent);
+
+    //increase decrease quantity button
+    var indeButton = document.createElement('div');
+    indeButton.className = "indeButton";
+    indeButton.style = "height:15%;width:100%;"
+    indeButton.draggable = false;
+    shopItem.appendChild(indeButton);
+
+    var inButton = document.createElement('div');
+    inButton.className = "inButton";
+    inButton.style = "height:100%;width:"+h/3+"px;background-color:green;display:inline-block;";
+    inButton.draggable = false;
+    indeButton.appendChild(inButton);
+
+    var quanText = document.createElement('div');
+    quanText.className = "quanText";
+    quanText.style = "height:100%;width:"+h/3+"px;background-color:grey;display:inline-block;";
+    quanText.draggable = false;
+    indeButton.appendChild(quanText);
+
+    var deButton = document.createElement('div');
+    deButton.className = "inButton";
+    deButton.style = "height:100%;width:"+h/3+"px;background-color:red;display:inline-block;"
+    deButton.draggable = false;
+    indeButton.appendChild(deButton);
 
     //create a drag and drop overlay
     var overlay = document.getElementById("overlay");
     shopItem.addEventListener("drag", function(){overlay.style = "display:inherit;";});
     shopItem.addEventListener("dragend", function(){overlay.style = "display:none;";});
+
+    //append the item into the order item list
+    document.getElementById('cartList').appendChild(shopItem);
 }
