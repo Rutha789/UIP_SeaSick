7ece88d7f6482fefb66e7839299f6e4c6e2362bc
unknown <combiner8761@gmail.com>
2020-02-28 01:23:55 +0100

    Language preserved in localStorage, made relocalization of a page possible

--- js/localization.js
+++ js/localization.js
@@ -19,7 +19,34 @@ let swedishTranslationMap = {
     menu_filter: "Filtrera",
 };
 
-var applicationLanguage = "english";
+// Will update the applicationLanguage to that of localStorage, and
+// return it. You shouldn't need to use this; this module already
+// loads the langauge from localStorage automatically
+function loadLanguage() {
+    applicationLanguage = localStorage.getItem('language');
+    return applicationLanguage;
+}
+
+var applicationLanguage = undefined;
+
+// Initialize the language to what we've stored in localStorage
+loadLanguage();
+
+// If we haven't stored the language in localStorage,
+// default it to english.
+if (typeof applicationLanguage === 'undefined') {
+    setLanguage("english");
+}
+
+// Changes the language of the application to that of the argument,
+// updating localStorage with it.
+//
+// Obs! In order for change in language to become visible in the page,
+// call "localizePage()" after "setLanguage()".
+function setLanguage(lang) {
+    applicationLanguage = lang;
+    localStorage.setItem("language", lang);
+}
 
 function localizedString(string) {
     if (applicationLanguage === "english") {
@@ -42,21 +69,56 @@ function validLocalizedKey(string) {
 // text occurences of a valid key for a localized string with
 // the localized string.
 //
-// Note: the "localize" class will hide all text in the DOM element until
-// localizePage() is run.
+// Even if the elements have already been localized, this function will
+// relocalize them. This is useful if you've switched languages, and want to
+// update the page to use the new language.
+// Simply call "localizePage()" again after "setLanguage()".
 //
-// Note: this removes the "localize" class from each DOM object traversed.
-// If you need to use this multiple times on the same page, re-add "localize"
-// as a class to the elements you need to localize and then run
-// localizePage(). Alternatively, use localizeText() on the text content
-// of each element you need translated..
+// Note: the "localize" class will hide all text in the DOM elements until
+// localizePage() is run.
 function localizePage() {
     $(".localize").each(function () {
-        $(this).text(localizeText($(this).text()));
-        $(this).removeClass("localize");
+        localizeDOM(this);
     });
 }
 
+// Localizes a DOM element, replacing all
+// text occurences of a valid key for a localized string with
+// the localized string.
+//
+// Even if the element has already been localized, this function will
+// relocalize it. This is useful if you've switched languages, and want to
+// update the element to use the new language.
+//
+// This will add the "localize" class to the DOM element if it didn't
+// have it already, so that localizePage() will visit it upon relocalization.
+function localizeDOM(dom) {
+    // Give the element the "localize" class if it doesn't have it already,
+    // so "localizePage()" will visit it if called.
+    $(dom).addClass("localize");
+    // Since localizeDOM replaces the original text of the dom, we need to
+    // preserve the original text for relocalization (switching language).
+    let text = $(dom).data("originalText");
+    // A check for if this is a initial localization or a relocalization.
+    // - If it's relocalization, we use .data("originalText") rather than
+    //   .text()
+    // - If it's an initial localization, use .text() and set
+    //   .data("originalText")
+    if (typeof text === "undefined") {
+        text = $(dom).text();
+        $(dom).data("originalText", text);
+    }
+    $(dom).text(localizeText(text));
+    // There's a rule that makes text of "localize"-classed DOM elements
+    // invisible. It doesn't apply if the DOM element has the "localized"
+    // class. So by adding this class, we make text of the targeted DOM
+    // element visible again.
+    $(dom).addClass("localized");
+}
+
+// Localizes a piece of text, replacing all
+// text occurences of a valid key for a localized string with
+// the localized string.
 function localizeText(text) {
     let toStudy = text.split("\n").map(l => l.split(" "));
     for (let lineIx in toStudy) {
