f62b119694b64877bcb2a18908f4d74d9acd1858
tokeifung14 <tokeifung14@gmail.com>
2020-02-29 11:09:47 +0100

    combine orderBar and shop menu

--- js/orderBar.js
+++ js/orderBar.js
@@ -1,98 +1,29 @@
 $(document).ready(function(){
-    var h = document.getElementById('cartList').clientHeight;
+    setTimeout(function(){
 
-    var itemList;
-    itemList = localStorage.item;
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
-    renderOrderItem(h,itemList[0].itemId,itemList[0].quan);
+        var h = document.getElementById('cartList').clientHeight;
+        var itemList;
+        itemList = localStorage.item;
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+        addDOMItemToOrderBar(renderItem(h,itemList[0],"orderBar"));
+    },1000);
 });
 
-function renderOrderItem(h,itemId,quan = 1, pc = "40%"){
-
-    //render shopitem regarding the size
-    var shopItem = document.createElement('div');
-    shopItem.className = "shopItem";
-    shopItem.style = "height:"+h+"px;width:"+h+"px;display: inline-block;padding:1% 1% 1% 1%;background-color: black;";
-    shopItem.draggable = true;
-    shopItem.ondrag = true;
-    if(itemId != undefined){shopItem.id = itemId;}
-
-    //create a infoicon for each shopItem
-    var infoIcon = document.createElement("img");
-    infoIcon.className = "infoIcon";
-    infoIcon.style = "height:20%;width: 20%;top:0;margin: 2% 2% 2% 2%;";
-    infoIcon.draggable = false;
-    infoIcon.src = "../res/info_icon.png";
-    shopItem.appendChild(infoIcon);
-
-    //create a ecoIcon
-    var ecoIcon = document.createElement("img");
-    ecoIcon.className = "ecoIcon";
-    ecoIcon.style = "height:20%;width: 20%;bottom:0;margin: 2% 2% 2% 2%;";
-    ecoIcon.draggable = false;
-    ecoIcon.src = "../res/eco_icon.png";
-    shopItem.appendChild(ecoIcon);
-
-    //create a pcent
-    var pcent = document.createElement("p");
-    pcent.className = "pcent";
-    pcent.textContent = pc;
-    pcent.style = "height:20%;width: 20%;margin: 2% 2% 2% 2%;color:white;font-weight: bold;";
-    pcent.draggable = false;
-    shopItem.appendChild(pcent);
-
-    //increase decrease quantity button
-    var indeButton = document.createElement('div');
-    indeButton.className = "indeButton";
-    indeButton.style = "height:15%;width:100%;display:inline-block;"
-    indeButton.draggable = false;
-    shopItem.appendChild(indeButton);
-
-    var inButton = document.createElement('div');
-    inButton.className = "inButton";
-    inButton.style = "height:100%;width:"+h/3+"px;background-color:green;display:inline-block;float:left;text-align: center;font-weight: bolder;font-size: larger;";
-    inButton.draggable = false;
-    inButton.textContent= "+";
-    indeButton.appendChild(inButton);
-
-    var quanText = document.createElement('div');
-    quanText.className = "quanText";
-    quanText.style = "height:100%;width:"+h/3+"px;background-color:grey;display:inline-block;text-align: center;font-weight: bolder;font-size: larger;";
-    quanText.draggable = false;
-    quanText.textContent= quan;
-    indeButton.appendChild(quanText);
-
-    var deButton = document.createElement('div');
-    deButton.className = "inButton";
-    deButton.style = "height:100%;width:"+h/3+"px;background-color:red;display:inline-block;float:right;text-align: center;font-weight: bolder;font-size: larger;"
-    deButton.draggable = false;
-    deButton.textContent= "-";
-    indeButton.appendChild(deButton);
-
-    //create a drag and drop overlay
-    var overlay = document.getElementById("overlay");
-    shopItem.addEventListener("drag", function(){overlay.style = "display:inherit;";});
-    shopItem.addEventListener("dragend", function(){overlay.style = "display:none;";});
-
-    //append the item into the order item list
-    document.getElementById('cartList').appendChild(shopItem);
+function addDOMItemToOrderBar(dom) {
+    document.getElementById('cartList').appendChild(dom);
 }
--- js/orderMenu.js
+++ js/orderMenu.js
@@ -1,71 +1,25 @@
 window.onload = function() {
-    initialPage();
+    initialOrderMenu();
+    initialOrderBar();
 };
 
-function initialPage(){
-    $('#orderBar').load('../html/orderBar.html');
-
+function initialOrderMenu(){
     var h = document.getElementById('item-container').clientHeight/3;
-    var itemList;
-    itemList = localStorage.item;
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
-    renderShopItem(h,itemList[0].itemId,itemList[0].quan);
+    // Load the drink database asynchronously. Once it's been loaded,
+    // display the first 10 items in the menu.
+    loadDB().then(function (dataBase) {
+        $("#item-container").html("");
+        for (let i = 0; i < 10; i++) {
+            addDOMItemToMenu(renderItem(h,dataBase[i],"orderMenu"));
+        }
+    });
+}
+
+function initialOrderBar(){
+    $('#orderBar').load('../html/orderBar.html');
 };
 
-function renderShopItem(h,itemId,quan = 1, pc = "40%"){
-
-    //render shopitem regarding the size
-    var shopItem = document.createElement('div');
-    shopItem.className = "shopItem";
-    shopItem.style = "height:"+h+"px;width:"+h+"px;display: inline-block;padding:1% 1% 1% 1%;background-color:dimgrey;";
-    shopItem.draggable = true;
-    shopItem.ondrag = true;
-    if(itemId != undefined){shopItem.id = itemId;}
-
-    //create a infoicon for each shopItem
-    var infoIcon = document.createElement("img");
-    infoIcon.className = "infoIcon";
-    infoIcon.style = "height:20%;width: 20%;top:0;margin: 2% 2% 2% 2%;";
-    infoIcon.draggable = false;
-    infoIcon.src = "../res/info_icon.png";
-    shopItem.appendChild(infoIcon);
-
-    //create a ecoIcon
-    var ecoIcon = document.createElement("img");
-    ecoIcon.className = "ecoIcon";
-    ecoIcon.style = "height:20%;width: 20%;bottom:0;margin: 2% 2% 2% 2%;";
-    ecoIcon.draggable = false;
-    ecoIcon.src = "../res/eco_icon.png";
-    shopItem.appendChild(ecoIcon);
-
-    //create a pcent
-    var pcent = document.createElement("p");
-    pcent.className = "pcent";
-    pcent.textContent = pc;
-    pcent.style = "height:20%;width: 20%;margin: 2% 2% 2% 2%;color:white;font-weight: bold;";
-    pcent.draggable = false;
-    shopItem.appendChild(pcent);
-
-    //create a drag and drop overlay
-    var overlay = document.getElementById("overlay");
-    shopItem.addEventListener("drag", function(){overlay.style = "display:inherit;";});
-    shopItem.addEventListener("dragend", function(){overlay.style = "display:none;";});
-
-    //append the item into the order item list
-    document.getElementById('item-container').appendChild(shopItem);
+// Given a DOM element, appends it to the order menu.
+function addDOMItemToMenu(dom) {
+    $("#item-container").append(dom);
 }
