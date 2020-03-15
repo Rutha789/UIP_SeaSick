0ff1ec3ba86809e5d8702e6de78f8a54de07efba
josiyoni <josiyoniug@gmail.com>
2020-03-15 14:53:00 +0100

    Temporaries

--- js/orderBar.js
+++ js/orderBar.js
@@ -1,64 +1,86 @@
-$(document).ready(function(){
-    cartListHeight = document.getElementById('cartList').clientHeight;
-    undoManager.registerCallback(renderOrderBar);
-    resizeButton();
-    $("#clearButton").click( function(event){
-        if(orderList.length() > 0){undoManager.perform(orderList.clearCommand())};
-        }
-    )
+$(document).ready(function() {
+  cartListHeight = document.getElementById("cartList").clientHeight;
+  undoManager.registerCallback(renderOrderBar);
+  resizeButton();
+  $("#clearButton").click(function(event) {
+    if (orderList.length() > 0) {
+      undoManager.perform(orderList.clearCommand());
+    }
+  });
 });
 
 function addDOMItemToOrderBar(dom) {
-    document.getElementById('cartList').appendChild(dom);
+  document.getElementById("cartList").appendChild(dom);
 }
 
-function onDropOrderList(event){
-    const serializedItem = event.dataTransfer.getData('item');
-    if (serializedItem !== null) {
-        const item = Item.fromJSONString(serializedItem);
-        undoManager.perform(orderList.addItemCommand(item));
-    }
+function onDropOrderList(event) {
+  const serializedItem = event.dataTransfer.getData("item");
+  if (serializedItem !== null) {
+    const item = Item.fromJSONString(serializedItem);
+    undoManager.perform(orderList.addItemCommand(item));
+  }
 }
 
 function renderOrderBar() {
-    $("#cartList").html("");
-    for (let itemQuantity of orderList) {
-        const domElem = itemQuantity.renderForOrderList(cartListHeight);
-        addDOMItemToOrderBar(domElem);
-    }
+  $("#cartList").html("");
+  for (let itemQuantity of orderList) {
+    const domElem = itemQuantity.renderForOrderList(cartListHeight);
+    addDOMItemToOrderBar(domElem);
+  }
 }
 
 function allowDropOrderList(event) {
-    event.preventDefault();
-
+  event.preventDefault();
 }
 
 function allowDropOrderMenu(event) {
-    event.preventDefault();
-    console.info("allowDropOrderMenu");
-
+  event.preventDefault();
+  console.info("allowDropOrderMenu");
 }
 
-function shopItemOnDrag(event){
-    event.dataTransfer.setData("item", $(event.target).data("item").toJSONString());
+function shopItemOnDrag(event) {
+  event.dataTransfer.setData(
+    "item",
+    $(event.target)
+      .data("item")
+      .toJSONString()
+  );
 }
 
-function removeItem(event){
-    const serializedItem = event.dataTransfer.getData('item');
-    if (serializedItem !== null) {
-        const item = Item.fromJSONString(serializedItem);
-        let quan = orderList.items[item.nr].quantity;
-        undoManager.perform(orderList.removeItemCommand(item.nr,quan));
-    }
+function removeItem(event) {
+  const serializedItem = event.dataTransfer.getData("item");
+  if (serializedItem !== null) {
+    const item = Item.fromJSONString(serializedItem);
+    let quan = orderList.items[item.nr].quantity;
+    undoManager.perform(orderList.removeItemCommand(item.nr, quan));
+  }
 }
 
-function resizeButton(){
-    var orderButtonBox = document.getElementById("orderButtonBox");
-    orderButtonBox.style.height = cartListHeight + "px";
-    var confirmButton = document.getElementById("confirmButton");
-    confirmButton.style.height = cartListHeight/2 + "px";
-    confirmButton.style.width = cartListHeight/2 + "px";
-    var clearButton = document.getElementById("clearButton");
-    clearButton.style.height = cartListHeight/2 + "px";
-    clearButton.style.width = cartListHeight/2 + "px";
+function resizeButton() {
+  var orderButtonBox = document.getElementById("orderButtonBox");
+  orderButtonBox.style.height = cartListHeight + "px";
+  var confirmButton = document.getElementById("confirmButton");
+  confirmButton.style.height = cartListHeight / 2 + "px";
+  confirmButton.style.width = cartListHeight / 2 + "px";
+  var clearButton = document.getElementById("clearButton");
+  clearButton.style.height = cartListHeight / 2 + "px";
+  clearButton.style.width = cartListHeight / 2 + "px";
 }
+document.getElementById("vip-user").addEventListener("click", function() {
+  let display = document.getElementById("vipcontainer-id");
+  if (display.style.visibility === "visible") {
+    display.style.visibility = "hidden";
+    document.getElementById("userid").value = "";
+    document.getElementById("userpassid").value = "";
+  } else {
+    display.style.visibility = "visible";
+  }
+});
+
+document.getElementById("vipclose-id").addEventListener("click", function() {
+  let e = document.getElementById("vipcontainer-id");
+
+  e.style.visibility = "hidden";
+  document.getElementById("userid").value = "";
+  document.getElementById("userpassid").value = "";
+});
--- js/orderMenuDevelop.js
+++ js/orderMenuDevelop.js
@@ -7,153 +7,144 @@ var orderBarPromise = undefined;
 var pageIx = 0;
 var pageSize = 10;
 var drinkMenuModel = undefined;
-var tableNum = Math.floor((Math.random() * 100) + 1);
-
+var tableNum = Math.floor(Math.random() * 100 + 1);
 
 $(document).ready(initialOrderMenu);
 
-function initialOrderList(){
-    const serialized = localStorage.getItem("orderList");
-    if (serialized === null) {
-        // object does not exist then create a new orderList
-        orderList = new OrderList();
-        localStorage.setItem("orderList",JSON.stringify(orderList));
-    } else {
-        orderList = OrderList.fromJSONString(serialized);
-    }
+function initialOrderList() {
+  const serialized = localStorage.getItem("orderList");
+  if (serialized === null) {
+    // object does not exist then create a new orderList
+    orderList = new OrderList();
+    localStorage.setItem("orderList", JSON.stringify(orderList));
+  } else {
+    orderList = OrderList.fromJSONString(serialized);
+  }
 }
 
 initialOrderList();
 
-function initialOrderBar(callback){
-    $('#orderBar').load('../html/orderBar.html', callback);
-};
+function initialOrderBar(callback) {
+  $("#orderBar").load("../html/orderBar.html", callback);
+}
 
-function initialOrderBarPromise(){
-    return new Promise(resolve => initialOrderBar(resolve));
-};
+function initialOrderBarPromise() {
+  return new Promise(resolve => initialOrderBar(resolve));
+}
 
 // Given a DOM element, appends it to the order menu.
 function addDOMItemToMenu(dom) {
-    $("#item-container").append(dom);
+  $("#item-container").append(dom);
 }
 
-function clickIf (DOMelem, condition, callback) {
-    if (condition) {
-        $(DOMelem).removeClass("unclickable");
-        $(DOMelem).click(callback);
-    } else {
-        $(DOMelem).addClass("unclickable");
-        $(DOMelem).off("click");
-    }
+function clickIf(DOMelem, condition, callback) {
+  if (condition) {
+    $(DOMelem).removeClass("unclickable");
+    $(DOMelem).click(callback);
+  } else {
+    $(DOMelem).addClass("unclickable");
+    $(DOMelem).off("click");
+  }
 }
 
-function renderPaymentScreen () {
-    $("#paydialog-id").css("display","block");
-    $("#overlay-id").css("display","block");
-    $(".pay-items").html("");
-    let total = 0;
-    for (const itemQ of orderList) {
-        total += itemQ.item.priceinclvat * itemQ.quantity;
-        $(".pay-items").append(itemQ.renderPayment());
-    }
-    $("#pay-total-amount").text(
-        localizedString("pay_total_cost") + " " + total + " SEK"
-   );
+function renderPaymentScreen() {
+  $("#paydialog-id").css("display", "block");
+  $("#overlay-id").css("display", "block");
+  $(".pay-items").html("");
+  let total = 0;
+  for (const itemQ of orderList) {
+    total += itemQ.item.priceinclvat * itemQ.quantity;
+    $(".pay-items").append(itemQ.renderPayment());
+  }
+  $("#pay-total-amount").text(
+    localizedString("pay_total_cost") + " " + total + " SEK"
+  );
 }
 
 function initialOrderMenu() {
-    orderBarPromise = initialOrderBarPromise();
-    orderBarPromise.then(function () {
-        undoManager.registerCallback(
-            () =>
-                clickIf(
-                    $("#confirmButton"),
-                    orderList.length() > 0,
-                    renderPaymentScreen
-                )
-        );
-        $("#close-pay").click(function() {
-            $("#paydialog-id").css("display","none");
-            $("#overlay-id").css("display","none");
-        });
-        $("#pay-bar").click(function () {
-            completeOrder({type: "bar"});
-        });
-        $("#pay-table").click(function () {
-            completeOrder({type: "table"});
-        });
+  orderBarPromise = initialOrderBarPromise();
+  orderBarPromise.then(function() {
+    undoManager.registerCallback(() =>
+      clickIf($("#confirmButton"), orderList.length() > 0, renderPaymentScreen)
+    );
+    $("#close-pay").click(function() {
+      $("#paydialog-id").css("display", "none");
+      $("#overlay-id").css("display", "none");
     });
-    $("#filter-form").submit(function(e) {
-        e.preventDefault();
-        if (typeof drinkMenuModel !== "undefined") {
-            submitFiltering();
-        }
+    $("#pay-bar").click(function() {
+      completeOrder({ type: "bar" });
     });
-    $("#toggle-filter-btn").click(function() {
-        $("#filter-menu").css(
-            "display",
-            (i,display) =>
-                display === "none" ? "grid" : "none"
-        );
+    $("#pay-table").click(function() {
+      completeOrder({ type: "table" });
     });
+  });
+  $("#filter-form").submit(function(e) {
+    e.preventDefault();
+    if (typeof drinkMenuModel !== "undefined") {
+      submitFiltering();
+    }
+  });
+  $("#toggle-filter-btn").click(function() {
+    $("#filter-menu").css("display", (i, display) =>
+      display === "none" ? "grid" : "none"
+    );
+  });
+  updatePage();
+  localizePage();
+  undoManager.registerCallback(updatePage);
+  $("#undo-button").click(() => undoManager.undo());
+  $("#redo-button").click(() => undoManager.redo());
+
+  // Load the drink database asynchronously. Once it's been loaded,
+  // display the first 10 items in the menu.
+  loadDB().then(function(dataBase) {
+    drinkMenuModel = new MenuModel(dataBase);
+
+    $("#item-container").html("");
+
+    for (let cat of $("#main-categories").children()) {
+      cat.onclick = () => setMainCategory(idToMainCat(cat.id));
+    }
     updatePage();
-    localizePage();
-    undoManager.registerCallback(updatePage);
-    $("#undo-button").click(() => undoManager.undo());
-    $("#redo-button").click(() => undoManager.redo());
-
-    // Load the drink database asynchronously. Once it's been loaded,
-    // display the first 10 items in the menu.
-    loadDB().then(function (dataBase) {
-        drinkMenuModel = new MenuModel(dataBase);
-
-        $("#item-container").html("");
-
-        for (let cat of $("#main-categories").children()) {
-            cat.onclick = () => setMainCategory(idToMainCat(cat.id));
-        }
-        updatePage();
-    });
-    $(".lang").click(() => changeLanguage(event));
-};
+  });
+  $(".lang").click(() => changeLanguage(event));
+}
 
 function nextPage() {
-    if (typeof drinkMenuModel === "undefined") {
-        return;
-    }
-    let filteredMenu =
-        drinkMenuModel
-        .getMenu()
-        .restricted((pageIx + 1)*pageSize,(pageIx+2)*pageSize);
-    if (filteredMenu.length() > 0) {
-        pageIx++;
-        updatePage();
-    }
+  if (typeof drinkMenuModel === "undefined") {
+    return;
+  }
+  let filteredMenu = drinkMenuModel
+    .getMenu()
+    .restricted((pageIx + 1) * pageSize, (pageIx + 2) * pageSize);
+  if (filteredMenu.length() > 0) {
+    pageIx++;
+    updatePage();
+  }
 }
 
 function prevPage() {
-    if (typeof drinkMenuModel === "undefined") {
-        return;
-    }
-    if (pageIx > 0) {
-        pageIx--;
-        updatePage();
-    }
+  if (typeof drinkMenuModel === "undefined") {
+    return;
+  }
+  if (pageIx > 0) {
+    pageIx--;
+    updatePage();
+  }
 }
 
-function completeOrder (method) {
-    let items = localStorage.getItem("registeredOrders");
+function completeOrder(method) {
+  let items = localStorage.getItem("registeredOrders");
 
-    if (items === null) {
-        items = [];
-    } else {
-        items = JSON.parse(items);
-    }
-    items.push({order: orderList, table: tableNum, method: method});
-    localStorage.setItem("registeredOrders",JSON.stringify(items));
-    localStorage.setItem("orderedList",JSON.stringify(orderList));
-    window.location = "../html/WowYouOrderedGoodJob.html";
+  if (items === null) {
+    items = [];
+  } else {
+    items = JSON.parse(items);
+  }
+  items.push({ order: orderList, table: tableNum, method: method });
+  localStorage.setItem("registeredOrders", JSON.stringify(items));
+  localStorage.setItem("orderedList", JSON.stringify(orderList));
+  window.location = "../html/WowYouOrderedGoodJob.html";
 }
 
 // $(document).on("submit", "#filter-form input[type='submit']", function () {
@@ -164,138 +155,149 @@ function completeOrder (method) {
 // });
 
 function updateUndoRedoButtons() {
-    $("#undo-button").css("opacity", 0.5 + undoManager.undoAvailable()*0.5);
-    $("#redo-button").css("opacity", 0.5 + undoManager.redoAvailable()*0.5);
+  $("#undo-button").css("opacity", 0.5 + undoManager.undoAvailable() * 0.5);
+  $("#redo-button").css("opacity", 0.5 + undoManager.redoAvailable() * 0.5);
 }
 
 function mainCatToId(cat) {
-    if (cat === null) {
-        cat = "ALL";
-    }
-    return "cat-" + cat;
+  if (cat === null) {
+    cat = "ALL";
+  }
+  return "cat-" + cat;
 }
 function idToMainCat(id) {
-    let cat = id.slice(4);
-    if (cat === "ALL") {
-        cat = null;
-    }
-    return cat;
+  let cat = id.slice(4);
+  if (cat === "ALL") {
+    cat = null;
+  }
+  return cat;
 }
 
 function setMainCategory(category) {
-    if (typeof drinkMenuModel === "undefined"
-        || drinkMenuModel.mainCategory === category) {
-        return;
-    }
-    $("#" + mainCatToId(drinkMenuModel.mainCategory)).removeClass("selected");
-    drinkMenuModel.mainCategory = category;
-    $("#" + mainCatToId(drinkMenuModel.mainCategory)).addClass("selected");
-    updatePage();
+  if (
+    typeof drinkMenuModel === "undefined" ||
+    drinkMenuModel.mainCategory === category
+  ) {
+    return;
+  }
+  $("#" + mainCatToId(drinkMenuModel.mainCategory)).removeClass("selected");
+  drinkMenuModel.mainCategory = category;
+  $("#" + mainCatToId(drinkMenuModel.mainCategory)).addClass("selected");
+  updatePage();
 }
 
 function submitFiltering() {
-    let action = drinkMenuModel
-        .modifyFilterCommand(getFilterForm)
-        .augment(new Command(
-            function () {
-                pageIx = 0;
-            },
-            function () {
-                setFilterForm(drinkMenuModel.filters);
-                pageIx = 0;
-            },
-            function () {
-                setFilterForm(drinkMenuModel.filters);
-                pageIx = 0;
-            }));
-    undoManager.perform(action);
+  let action = drinkMenuModel.modifyFilterCommand(getFilterForm).augment(
+    new Command(
+      function() {
+        pageIx = 0;
+      },
+      function() {
+        setFilterForm(drinkMenuModel.filters);
+        pageIx = 0;
+      },
+      function() {
+        setFilterForm(drinkMenuModel.filters);
+        pageIx = 0;
+      }
+    )
+  );
+  undoManager.perform(action);
 }
 
 function getFilterForm() {
-    let filters = emptyFilters();
-    for (let element of $("#filter-form input")) {
-        // slice(7) to remove "filter-" prefix of form element's id
-        let key = element.id.slice(7);
-        let studiedFilters = filters;
-        // If it's a filter option for drinks, the key exists in filters.drink.
-        // Drop that prefix also
-        if (key.startsWith("drink-")) {
-            studiedFilters = filters.drink;
-            key = key.slice(6);
-        }
-        if (key in studiedFilters) {
-            if (element.type === "checkbox") {
-                studiedFilters[key] = element.checked;
-            } else if (element.type === "number" && element.value !== "") {
-                studiedFilters[key] = Number(element.value);
-            } else if (element.type === "search") {
-                studiedFilters[key] = googlify(element.value);
-            } else if (element.type === "text") {
-                studiedFilters[key] = element.value;
-            };
-        }
+  let filters = emptyFilters();
+  for (let element of $("#filter-form input")) {
+    // slice(7) to remove "filter-" prefix of form element's id
+    let key = element.id.slice(7);
+    let studiedFilters = filters;
+    // If it's a filter option for drinks, the key exists in filters.drink.
+    // Drop that prefix also
+    if (key.startsWith("drink-")) {
+      studiedFilters = filters.drink;
+      key = key.slice(6);
     }
-    return filters;
+    if (key in studiedFilters) {
+      if (element.type === "checkbox") {
+        studiedFilters[key] = element.checked;
+      } else if (element.type === "number" && element.value !== "") {
+        studiedFilters[key] = Number(element.value);
+      } else if (element.type === "search") {
+        studiedFilters[key] = googlify(element.value);
+      } else if (element.type === "text") {
+        studiedFilters[key] = element.value;
+      }
+    }
+  }
+  return filters;
 }
 
 function setFilterForm(filters) {
-    for (let element of $("#filter-form input")) {
-        // slice(7) to remove "filter-" prefix of form element's id
-        let key = element.id.slice(7);
-        let studiedFilters = filters;
-        // If it's a filter option for drinks, the key exists in filters.drink.
-        // Drop that prefix also
-        if (key.startsWith("drink-")) {
-            studiedFilters = filters.drink;
-            key = key.slice(6);
-        }
-        if (key in studiedFilters) {
-            if (element.type === "checkbox") {
-                element.checked = studiedFilters[key];
-            } else if (element.type === "number") {
-                element.value = studiedFilters[key];
-            } else if (element.type === "search") {
-                element.value =
-                    studiedFilters[key]
-                    .map(str => str.includes(" ") ? "\" + str + \"" : str)
-                    .join(" ");
-            } else if (element.type === "text") {
-                element.value = studiedFilters[key];
-            };
-        }
+  for (let element of $("#filter-form input")) {
+    // slice(7) to remove "filter-" prefix of form element's id
+    let key = element.id.slice(7);
+    let studiedFilters = filters;
+    // If it's a filter option for drinks, the key exists in filters.drink.
+    // Drop that prefix also
+    if (key.startsWith("drink-")) {
+      studiedFilters = filters.drink;
+      key = key.slice(6);
     }
+    if (key in studiedFilters) {
+      if (element.type === "checkbox") {
+        element.checked = studiedFilters[key];
+      } else if (element.type === "number") {
+        element.value = studiedFilters[key];
+      } else if (element.type === "search") {
+        element.value = studiedFilters[key]
+          .map(str => (str.includes(" ") ? '" + str + "' : str))
+          .join(" ");
+      } else if (element.type === "text") {
+        element.value = studiedFilters[key];
+      }
+    }
+  }
 }
 
 function updatePage() {
-    updateUndoRedoButtons();
-    itemContainerWidth = document.getElementById('item-container').clientWidth;
-    if (typeof drinkMenuModel === "undefined"
-     || typeof orderBarPromise === "undefined") {
-        return;
+  updateUndoRedoButtons();
+  itemContainerWidth = document.getElementById("item-container").clientWidth;
+  if (
+    typeof drinkMenuModel === "undefined" ||
+    typeof orderBarPromise === "undefined"
+  ) {
+    return;
+  }
+  orderBarPromise.then(function() {
+    let filteredMenu = drinkMenuModel
+      .getMenu()
+      .restricted(pageIx * pageSize, (pageIx + 1) * pageSize);
+    $("#item-container").html("");
+    for (let item of filteredMenu) {
+      addDOMItemToMenu(item.renderForMenu(itemContainerWidth / 4));
     }
-    orderBarPromise.then(function () {
-        let filteredMenu =
-            drinkMenuModel
-            .getMenu()
-            .restricted(pageIx*pageSize,(pageIx+1)*pageSize);
-        $("#item-container").html("");
-        for (let item of filteredMenu) {
-            addDOMItemToMenu(item.renderForMenu(itemContainerWidth/4));
-        }
-    });
+  });
 }
 
 // Given a DOM element, appends it to the order menu.
 function addDOMItemToMenu(dom) {
-    $("#item-container").append(dom);
+  $("#item-container").append(dom);
 }
 
-function langOptionShow(){
-    console.info("hihi");
+// document
+//   .getElementById("language-button")
+//   .addEventListener("click", function() {
+//     document.getElementById("language-options").classList.add("show");
+//   });
+function langOptionShow() {
+  if (document.querySelector(".show" === null)) {
     document.getElementById("language-options").classList.add("show");
+  } else {
+    document.getElementById("language-options").classList.remove("show");
+  }
 }
 
-function changeLanguage(event){
-    console.info(event.target.id);
-    setLanguage(event.target.id);
+function changeLanguage(event) {
+  console.info(event.target.id);
+  setLanguage(event.target.id);
 }
