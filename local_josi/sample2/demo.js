3b06da62dcc449d63dc674c8d6ab835feec51da4
unknown <combiner8761@gmail.com>
2020-03-20 11:07:48 +0100

    Merge sonasri's changes.Delete worthless folders

--- local_josi/sample2/demo.js
+++
@@ -1,8 +0,0 @@
-const people = ["josi", "yoni", "nor", "naiomi", "welid", "meron"];
-const ulpeople = document.querySelector(".person");
-
-let htmlli = ``;
-
-people.forEach(person => {
-  ulpeople.innerHTML += `<li style="color:purple">${person}</li>`;
-});
